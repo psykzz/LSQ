f51797461803f682c6320211da04f9b3dec9eb8b
Matt Smith <matt.daemon660@gmail.com>
2015-04-06 12:10:29 +0100

    commit initial test

---
+++ tests/lsq_tests.py
@@ -0,0 +1,21 @@
+import os
+import unittest
+import tempfile
+
+# Import the application
+from app import app as lsq
+
+class LSQTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = lsq.app.test_client()
+
+    def tearDown(self):
+        pass
+        
+    def test_homepage_200(self):
+        rv = self.app.get('/')
+        assert 200 == rv.status_code
+
+if __name__ == '__main__':
+    unittest.main()
